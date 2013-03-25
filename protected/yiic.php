d16cfe015f00d39ed02e0c6c69f6a1dd028364ed
Dale Scott <dale@dalescott.net>
2013-03-25 12:26:13 -0600

    Edited to use ../yii-1.1.13 (instead of YiiRoot)

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../YiiRoot/framework/yiic.php';
+//$yiic=dirname(__FILE__).'/../../YiiRoot/framework/yiic.php';
+$yiic=dirname(__FILE__).'/../../yii-1.1.13/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
